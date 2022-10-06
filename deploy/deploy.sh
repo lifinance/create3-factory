926cf0c0af6e3a44422618dd3c2cdf11b210b394
Ed Zynda <ezynda3@gmail.com>
2022-10-06 16:56:48 +0300

    deploy to all LIFI supported chains

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 deploy() {
 	NETWORK=$1
 
-	RAW_RETURN_DATA=$(forge script script/Deploy.s.sol -f $NETWORK -vvvv --json --silent --broadcast --verify --skip-simulation)
+	RAW_RETURN_DATA=$(forge script script/Deploy.s.sol -f $NETWORK -vvvv --json --silent --broadcast --verify --skip-simulation --legacy)
 	RETURN_DATA=$(echo $RAW_RETURN_DATA | jq -r '.returns' 2> /dev/null)
 
 	factory=$(echo $RETURN_DATA | jq -r '.factory.value')
