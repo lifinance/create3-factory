57c63c8f6a7292632f6fd7a676ca3d0271a8a9c1
Ed Zynda <ezynda3@gmail.com>
2024-06-07 09:57:35 +0300

    deploy to rootstock

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,8 +3,8 @@
 deploy() {
 	NETWORK=$1
 
-	RAW_RETURN_DATA=$(forge script script/Deploy.s.sol -f $NETWORK -vvvv --json --silent --broadcast --verify --skip-simulation --legacy)
-	RETURN_DATA=$(echo $RAW_RETURN_DATA | jq -r '.returns' 2> /dev/null)
+	RAW_RETURN_DATA=$(forge script script/Deploy.s.sol -f $NETWORK -vvvv --json --silent --broadcast --verify --legacy)
+	RETURN_DATA=$(echo $RAW_RETURN_DATA | jq -r '.returns' 2>/dev/null)
 
 	factory=$(echo $RETURN_DATA | jq -r '.factory.value')
 
